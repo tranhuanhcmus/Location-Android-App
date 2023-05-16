c52e52098200cded7c7ae740cbc597cde288ff1c
luckaruto <lucmten@gmail.com>
2023-05-16 09:32:22 +0700

    commit moi giao dien tinh nang

---
+++ GoogleMaps-2/app/src/main/java/com/example/googlemaps/Fragments/BottomSheetFragmentUsernameAndBioUpdate.java
@@ -0,0 +1,118 @@
+package com.example.googlemaps.Fragments;
+
+
+import static com.google.maps.android.Context.getApplicationContext;
+
+import android.app.FragmentManager;
+import android.content.Context;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.googlemaps.R;
+import com.example.googlemaps.model.User;
+import com.example.googlemaps.model.UserClient;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
+
+public class BottomSheetFragmentUsernameAndBioUpdate extends BottomSheetDialogFragment {
+    Context context;
+    Boolean isUsername;
+
+    EditText et_user_input_bottom_sheet_fragment;
+    TextView btnSave;
+    User user;
+    TextView btnCancel;
+    private FirebaseFirestore mDb;
+    String username;
+    String bio;
+
+    public BottomSheetFragmentUsernameAndBioUpdate(Context context, Boolean isUsername) {
+        this.context = context;
+        this.isUsername = isUsername;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(BottomSheetDialogFragment.STYLE_NORMAL, R.style.CustomBottomSheetDialogTheme);
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_bottom_sheet_bio_username_update, container, false);
+        Objects.requireNonNull(Objects.requireNonNull(getDialog()).getWindow()).setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        view.findViewById(R.id.et_user_input_bottom_sheet_fragment).requestFocus();
+        init(view);
+
+        return view;
+    }
+
+    private void updateUsernameAndBio() {
+
+        if(isUsername){
+            username = et_user_input_bottom_sheet_fragment.getText().toString().trim();
+            user.setUsername(username);
+            mDb.collection(getString(R.string.collection_users))
+                    .document(FirebaseAuth.getInstance().getUid())
+                    .set(user);
+            dismiss();
+        }else{
+            bio = et_user_input_bottom_sheet_fragment.getText().toString().trim();
+            user.setBio(bio);
+            mDb.collection(getString(R.string.collection_users))
+                    .document(FirebaseAuth.getInstance().getUid())
+                    .set(user);
+            dismiss();
+        }
+
+
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        user = ((UserClient) getActivity().getApplication()).getUser();
+    }
+
+    private void init(View view) {
+
+        mDb = FirebaseFirestore.getInstance();
+        et_user_input_bottom_sheet_fragment = view.findViewById(R.id.et_user_input_bottom_sheet_fragment);
+        btnSave = view.findViewById(R.id.btn_save_bottom_sheet);
+        btnCancel = view.findViewById(R.id.btn_cancel_bottom_sheet);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUsernameAndBio();
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+
+    }
+
+
+
+}
---
+++ GoogleMaps-2/app/src/main/java/com/example/googlemaps/Profile.java
@@ -0,0 +1,300 @@
+package com.example.googlemaps;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+
+import com.bumptech.glide.Glide;
+
+import com.example.googlemaps.Adapter.ChatMessageRecyclerAdapter;
+import com.example.googlemaps.Fragments.BottomSheetFragmentUsernameAndBioUpdate;
+import com.example.googlemaps.Fragments.UserListFragment;
+import com.example.googlemaps.model.Message;
+import com.example.googlemaps.model.User;
+import com.example.googlemaps.model.UserClient;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import xyz.hasnat.sweettoast.SweetToast;
+
+import static android.app.Activity.RESULT_OK;
+import static android.content.ContentValues.TAG;
+import static com.example.googlemaps.utils.ImageLoader.loadImageChatMessage;
+
+public class Profile extends AppCompatActivity implements View.OnClickListener {
+    Context context;
+
+    byte[] dataImageByte;
+    TextView tv_currentUserName_profile_fragment;
+    CircleImageView iv_profileImage_profile_fragment;
+    ImageView btn_profile_image_change;
+    ImageView btn_save_edit_user_name;
+    TextView tv_profile_fragment_bio;
+
+    String username;
+    String imageUrl;
+    String userBio;
+    String userId;
+    User user;
+    private String download_url;
+    private static final int IMAGE_REQUEST = 1;
+    private Uri imageUri;
+    String timeStamp;
+    @SuppressWarnings("rawtypes")
+    private StorageTask uploadImageTask;
+    private StorageReference imageMessageStorageRef;
+    private StorageReference fileReference;
+
+    Boolean isUsername;
+
+    private FirebaseFirestore mDb;
+
+    public Profile(Context context) {
+
+        this.context = context;
+    }
+    public Profile() {
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case android.R.id.home:{
+                finish();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private void initSupportActionBar() {
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#000000")));
+        setTitle("Profile");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_2);
+
+        tv_currentUserName_profile_fragment = findViewById(R.id.tv_profile_fragment_username);
+
+        tv_profile_fragment_bio = findViewById(R.id.tv_profile_fragment_bio);
+        tv_profile_fragment_bio.setOnClickListener(this);
+        iv_profileImage_profile_fragment = findViewById(R.id.iv_profile_fragment);
+
+        findViewById(R.id.iv_profile_fragment).setOnClickListener(this);
+
+        findViewById(R.id.btn_profile_image_change).setOnClickListener(this);
+
+        findViewById(R.id.btn_save_edit_username).setOnClickListener(this);
+
+
+
+        mDb = FirebaseFirestore.getInstance();
+        imageMessageStorageRef = FirebaseStorage.getInstance().getReference().child("messages_image");
+        fetchCurrentUserdata();
+        initSupportActionBar();
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
+    private void fetchCurrentUserdata() {
+        user = ((UserClient) (getApplicationContext())).getUser();
+
+        Log.d(TAG, user.getUsername()+ "dcm may");
+        if (user != null) {
+            username = user.getUsername();
+            Log.d(TAG, username+ "dcm doan anh may");
+           tv_currentUserName_profile_fragment.setText(username);
+            imageUrl = user.getAvatar();
+            if(user.getBio() != null) {
+
+                userBio = user.getBio();
+                tv_profile_fragment_bio.setText(userBio);
+
+            }
+            else{
+                tv_profile_fragment_bio.setText("");
+            }
+            userId = user.getUser_id();
+            //iv_profileImage_profile_fragment.setImageResource(R.drawable.placeholder_image_chat);
+            if (imageUrl.equals("default")) {
+                iv_profileImage_profile_fragment.setImageResource(R.drawable.placeholder_image_chat);
+
+            } else {
+                loadImageChatMessage(iv_profileImage_profile_fragment,imageUrl);
+            }
+        } else {
+            Toast.makeText(context, "User not found..", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+    private void openImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, IMAGE_REQUEST);
+    }
+
+
+    private void uploadImage() {
+
+        if (imageUri != null) {
+            final StorageReference file_path = imageMessageStorageRef.child(userId + ".jpg");
+            UploadTask uploadTask = file_path.putFile(imageUri);
+            Task<Uri> uriTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    if (!task.isSuccessful()) {
+                        SweetToast.error(Profile.this, "Error: " + task.getException().getMessage());
+                    }
+                    download_url = file_path.getDownloadUrl().toString();
+                    return file_path.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()) {
+                        if (task.isSuccessful()) {
+                            download_url = task.getResult().toString();
+                            //Toast.makeText(ChatActivity.this, "From ChatActivity, link: " +download_url, Toast.LENGTH_SHORT).show();
+
+                            user.setAvatar(download_url);
+
+                            mDb.collection(getString(R.string.collection_users))
+                                    .document(FirebaseAuth.getInstance().getUid())
+                                    .set(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                            } else {
+                                                View parentLayout = findViewById(android.R.id.content);
+                                                Snackbar.make(parentLayout, "Something went wrong.", Snackbar.LENGTH_SHORT).show();
+                                            }
+                                           // progressDialog.dismiss();
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                            //progressDialog.dismiss();
+                                        }
+                                    });
+                            Log.e("tag", "Image sent successfully");
+                        } else {
+                            SweetToast.warning(Profile.this, "Failed to send image. Try again");
+                        }
+                    }
+                }
+            });
+        }
+
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imageUri = data.getData();
+            uploadImage();
+        }
+    }
+
+    private void openBottomSheet(Boolean isUsername) {
+        BottomSheetFragmentUsernameAndBioUpdate bottomSheetFragmentUsernameAndBioUpdate = new BottomSheetFragmentUsernameAndBioUpdate(context, isUsername);
+        assert getSupportFragmentManager() != null;
+        bottomSheetFragmentUsernameAndBioUpdate.show(getSupportFragmentManager(), "edit");
+
+
+
+
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_profile_image_change: {
+                openImage();
+                break;
+            }
+            case R.id.iv_profile_fragment: {
+                openImage();
+                break;
+            }
+            case R.id.btn_save_edit_username: {
+                isUsername = true;
+                openBottomSheet(true);
+                break;
+            }
+            case R.id.tv_profile_fragment_bio: {
+                isUsername = false;
+                openBottomSheet(false);
+                break;
+            }
+
+        }
+    }
+}
+
+
